db5908bb841196e7ed0773e64c6e7b4848c45970
Carsten Behring <carsten.behring@gmail.com>
2021-02-22 16:28:52 +0100

    cleaned up

--- src/scicloj/metamorph/ml.clj
+++ src/scicloj/metamorph/ml.clj
@@ -1,19 +1,8 @@
 (ns scicloj.metamorph.ml
-  (:require [tech.v3.dataset.modelling :as ds-mod]
-            [tech.v3.datatype.functional :as dfn]
-            [tech.v3.datatype.argops :as argops]
-            [tech.v3.dataset :as ds]
-            [tech.v3.datatype.errors :as errors]
+  (:require [tech.v3.dataset :as ds]
+           [tech.v3.datatype.functional :as dfn]
             [tech.v3.dataset.column-filters :as cf]
-            [tech.v3.ml.loss :as loss]
-            [clojure.tools.logging :as log]
-            [tech.v3.ml :as ml]
-            [tech.v3.ml.gridsearch :as ml-gs]
-            [pppmap.core :as ppp]
-            [tablecloth.api.split :as split]
-            ))
-
-
+            [tech.v3.datatype.errors :as errors]))
 
 (defn calc-ctx-with-loss [pipeline-fn loss-fn train-ds test-ds]
   (let [fitted-ctx (pipeline-fn {:metamorph/mode :fit  :metamorph/data train-ds})
@@ -29,155 +18,28 @@
      :loss loss}))
 
 
-(defn evaluate-model [ds pipe-create-fn pipe-options
-                      split-type split-options
-                      loss-fn
-                      {:keys [
-                              n-gridsearch
-                              n-result-models
-                              ]
-                       :or {
-                            n-gridsearch 75
-                            n-result-models 5}
-                       }]
-    (let [train-split-seq (split/split ds split-type split-options)
-          gs-seq (take n-gridsearch (ml-gs/sobol-gridsearch pipe-options))
 
-          evaluations
-          (flatten
-           (ppp/pmap-with-progress
-            "gridsearch"
-            (fn [options]
-              (let [pipe-fn (pipe-create-fn options)]
-
-                (map
-                 (fn [{:keys [train test]}]
-                   (assoc (calc-ctx-with-loss pipe-fn loss-fn train test)
-                          :options options
-                          :pipe-fn pipe-fn))
-                 train-split-seq)))
-            gs-seq))]
-      (->>
-       evaluations
-       (sort-by :avg-loss)
-       (take n-result-models)
-       )
-      ))
-
-
-
-
-
-        ;; train-split-seq (split/split ds split-type split-options)
-        ;; gs-seq (take n-gridsearch (ml-gs/sobol-gridsearch pipe-options))
-        ;; pipe-fn-seq (map pipe-create-fn gs-seq )
-
-(defn evaluate-model [pipe-fn-seq
+(defn evaluate-pipelines [pipe-fn-seq
                       train-test-split-seq
                       loss-fn]
   (->>
-   (for [pipe-fn pipe-fn-seq train-test-split train-test-split-seq]
-     (let [{:keys [train test]} train-test-split]
-       (assoc (calc-ctx-with-loss pipe-fn loss-fn train test)
-                :pipe-fn pipe-fn)))
-   flatten
-   (sort-by :loss)
-   ))
+   (for [pipe-fn pipe-fn-seq ]
+     (let [split-eval-result
+           (for [train-test-split train-test-split-seq]
+             (let [{:keys [train test]} train-test-split]
+               (assoc (calc-ctx-with-loss pipe-fn loss-fn train test)
+                      :pipe-fn pipe-fn)))
 
+           loss-vec (mapv :loss split-eval-result)
+           {min-loss :min
+            max-loss :max
+            avg-loss :mean} (dfn/descriptive-statistics [:min :max :mean] loss-vec)
 
-(defn train-split
-  "Train a model splitting the dataset using tech.v3.dataset.modelling/train-test-split
-  and then calculate the loss using loss-fn.  Loss is added to the model map under :loss.
-  * `loss-fn` defaults to loss/mae if target column is not categorical else defaults to
-  loss/classification-loss."
-  ([dataset pipeline-fn options loss-fn]
-   (let [{:keys [train-ds test-ds]} (ds-mod/train-test-split dataset options)]
-     (calc-ctx-with-loss pipeline-fn loss-fn train-ds test-ds)
-     )) )
-
-
-(defn do-k-fold [pipeline-fn loss-fn ds-seq]
-  (let [models (mapv (fn [{:keys [train-ds test-ds]}]
-                       (calc-ctx-with-loss pipeline-fn loss-fn train-ds test-ds))
-                     ds-seq)
-        loss-vec (mapv :loss models)
-        _ (println loss-vec)
-
-        {
-         min-loss :min
-         max-loss :max
-         avg-loss :mean}
-        (dfn/descriptive-statistics [:min :max :mean] loss-vec)
-        min-model-idx (argops/argmin loss-vec)]
-    (assoc (models min-model-idx)
-           :min-loss min-loss
-           :max-loss max-loss
-           :avg-loss avg-loss
-           :n-k-folds (count ds-seq))))
-
-
-
-
-(defn train-k-fold
-  "Train a model across k-fold datasets using tech.v3.dataset.modelling/k-fold-dataset
-  and then calculate the min,max,and avg across results using loss-fn.  Adds
-  :n-k-folds, :min-loss, :max-loss, :avg-loss and :loss (min-loss) to the
-  model with the lowest loss.
-  * `n-k-folds` defaults to 5.
-  * `loss-fn` defaults to loss/mae if target column is not categorical else defaults to
-     loss/classification-loss."
-
-  ([dataset pipeline-fn n-k-folds loss-fn]
-   (do-k-fold pipeline-fn loss-fn
-              (ds-mod/k-fold-datasets dataset n-k-folds)
-              ))
-  ([dataset pipeline-fn n-k-folds]
-   (train-k-fold pipeline-fn dataset  n-k-folds (ml/default-loss-fn dataset)))
-  ([dataset pipeline-fn]
-   (train-k-fold  pipeline-fn dataset 5 (ml/default-loss-fn dataset))))
-
-
-(defn- pprint-to-string [o]
-  (let [out (java.io.StringWriter.)]
-    (clojure.pprint/pprint o out)
-    (.toString out)))
-
-(defn- safe-do-k-fold
-  [pipeline-fn loss-fn ds-seq]
-  (try
-    (do-k-fold pipeline-fn loss-fn ds-seq)
-    (catch Exception e
-      (log/error e "Exception caught during grid search "))))
-
-
-
-(defn train-auto-gridsearch
-  "Train a model gridsearching across the options map.  The gridsearch map is built by
-  merging the model's hyperparameter definitions into the options map.  If the sobol
-  sequence returned has only one element a warning is issued.  Note this returns a
-  sequence of models as opposed to a single model.
-  * Searches across k-fold datasets if n-k-folds is > 1.  n-k-folds defaults to 5.
-  * Searches (in parallel) through n-gridsearch option maps created via
-    sobol-gridsearch.
-  * Returns n-result-models (defaults to 5) sorted by avg-loss.
-  * loss-fn can be provided or is the loss-fn returned via default-loss-fn."
-  ([dataset pipeline-create-fn options {:keys [n-k-folds
-                            n-gridsearch
-                            n-result-models
-                            loss-fn]
-                     :or {n-k-folds 5
-                          n-gridsearch 75
-                          n-result-models 5}
-                     :as gridsearch-options}]
-
-   (let [gs-seq (take n-gridsearch (ml-gs/sobol-gridsearch options))
-         _ (when (== 1 (count gs-seq))
-             (log/warn "Did not find any gridsearch axis in options map"))
-         ds-seq (ds-mod/k-fold-datasets dataset n-k-folds gridsearch-options)]
-     (->> gs-seq
-          (ppp/ppmap-with-progress "gridsearch" 1 #(safe-do-k-fold (pipeline-create-fn  %) loss-fn ds-seq))
-          (sort-by :avg-loss)
-          (take n-result-models)
-          )))
-  ([dataset pipeline-create-fn  options]
-   (train-auto-gridsearch dataset pipeline-create-fn options nil)))
+           ]
+       (map
+        #(assoc %
+                :min-loss min-loss
+                :max-loss max-loss
+                :avg-loss avg-loss)
+        split-eval-result)))
+   flatten))
--- test/scicloj/metamorph/ml_test.clj
+++ test/scicloj/metamorph/ml_test.clj
@@ -10,138 +10,119 @@
              [tech.v3.ml.gridsearch :as gs]
              [scicloj.metamorph.ml :as ml-eval]
              [tech.v3.ml.loss :as loss]
-
-            [tech.v3.ml.gridsearch :as ml-gs]
-            [tablecloth.api.split :as split]
-            ))
-
-(defn pipe-create-fn [options]
-  (morph/pipeline
-   (ds-mm/set-inference-target :species)
-   (ds-mm/categorical->number cf/categorical)
-   (fn [ctx]
-     (assoc ctx
-            :target-ds (cf/target (:metamorph/data ctx))
-            )
-     )
-   (ml-mm/model (merge options
-                       {:model-type :smile.classification/random-forest
-                        }))))
+             [tech.v3.datatype.functional :as dtf]
+             [tech.v3.ml.gridsearch :as ml-gs]
+             [tablecloth.api.split :as split]
+             [tablecloth.api :as tc]
+             ))
 
 
 
-(deftest evaluate-model-simplest
-
-  (def ds (ds/->dataset "https://raw.githubusercontent.com/techascent/tech.ml/master/test/data/iris.csv" {:key-fn keyword}))
-
-  (defn pipe-create-fn [options]
-    (morph/pipeline
-     (ds-mm/set-inference-target :species)
-     (ds-mm/categorical->number cf/categorical)
-     (fn [ctx]
-       (assoc ctx
-              :target-ds (cf/target (:metamorph/data ctx))))
-     (ml-mm/model {:model-type :smile.classification/random-forest})))
-
-  (def res
-    (ml-eval/evaluate-model ds pipe-create-fn nil :holdout nil loss/classification-loss {}))
 
 
-  (def best-fitted-context  (-> res first :fitted-ctx))
-
-  (def best-pipe-fn (-> res first :pipe-fn))
-
-  (def new-ds (ds/sample ds 10 {:seed 1234} ))
-
-  (->
-   (best-pipe-fn
-    (merge best-fitted-context
-           {:metamorph/data new-ds
-            :metamorph/mode :transform}))
-   (:metamorph/data)
-   (ds-mod/column-values->categorical :species))
-
-  )
-
-(deftest grid-seach
+(deftest evaluate-pipelines-simplest
   (let [
-        ds
-        (ds/->dataset "https://raw.githubusercontent.com/techascent/tech.ml/master/test/data/iris.csv" {:key-fn keyword})
+        ;;  the data
+        ds (ds/->dataset "https://raw.githubusercontent.com/techascent/tech.ml/master/test/data/iris.csv" {:key-fn keyword})
 
+        ;;  the (single, fixed) pipe-fn
+        pipe-fn
+        (morph/pipeline
+         (ds-mm/set-inference-target :species)
+         (ds-mm/categorical->number cf/categorical)
+         (fn [ctx]
+           (assoc ctx
+                  :target-ds (cf/target (:metamorph/data ctx))))
+         (ml-mm/model {:model-type :smile.classification/random-forest}))
+
+        ;;  the simplest split
+        train-split-seq (split/split ds :holdout)
+
+        ;; one pipe-fn in the seq
+        pipe-fn-seq [pipe-fn]
+
+        evaluations
+        (ml-eval/evaluate-model pipe-fn-seq train-split-seq loss/classification-loss)
+
+
+        ;; we have only one result
+        best-fitted-context  (-> evaluations first :fitted-ctx)
+        best-pipe-fn         (-> evaluations first :pipe-fn)
+
+
+        ;;  simulate new data
+        new-ds (ds/sample ds 10 {:seed 1234} )
+
+        ;;  do prediction on new data
+        predictions
+        (->
+         (best-pipe-fn
+          (merge best-fitted-context
+                 {:metamorph/data new-ds
+                  :metamorph/mode :transform}))
+         (:metamorph/data)
+         (ds-mod/column-values->categorical :species))
+        ]
+
+    (is (= ["versicolor" "versicolor" "virginica" "versicolor" "virginica" "setosa" "virginica" "virginica" "versicolor" "versicolor" ]
+         predictions))))
+
+
+
+(deftest grid-search
+  (let [
+        ds(ds/->dataset "https://raw.githubusercontent.com/techascent/tech.ml/master/test/data/iris.csv" {:key-fn keyword})
 
         grid-search-options
         {:trees (gs/categorical [10 50 100 500])
          :split-rule (gs/categorical [:gini :entropy])
-         }
+         :model-type :smile.classification/random-forest}
+
+        create-pipe-fn (fn[options]
+                         (morph/pipeline
+                          (ds-mm/set-inference-target :species)
+                          (ds-mm/categorical->number cf/categorical)
+                          (fn [ctx]
+                            (assoc ctx
+                                   :target-ds (cf/target (:metamorph/data ctx))
+                                   ))
+     
+                          (ml-mm/model options)))
+
+        all-options (gs/sobol-gridsearch grid-search-options)
+        pipe-fn-seq (map create-pipe-fn all-options)
+        train-test-seq (split/split ds :kfold {:k 10})
+        evaluations (ml-eval/evaluate-pipelines pipe-fn-seq train-test-seq loss/classification-loss)
+
+        evalution-with-lowest-avg-loss
+        (->>
+         (group-by :pipe-fn evaluations)
+         vals
+         (map first)
+         (sort-by :avg-loss)
+         (first))
 
 
-        res
-        (ml-eval/train-auto-gridsearch
-         ds pipe-create-fn
-         grid-search-options
-         {:loss-fn loss/classification-loss} )]
+        new-ds (ds/sample ds 10 {:seed 1234} )
 
-    (map
-     #(-> % :avg-loss)
-     res)
-    ;; res
-    )
-  ;; (def fitted-ctx (pipe-fn {:metamorph/data (:train-ds split) :metamorph/mode :fit}))
-  ;; (def prediction-ctx (pipe-fn (merge fitted-ctx {:metamorph/data (:test-ds split) :metamorph/mode :transform})))
+        predictions
+        (->   ((evalution-with-lowest-avg-loss :pipe-fn)
+               (merge (evalution-with-lowest-avg-loss :fitted-ctx)
+                      {:metamorph/data new-ds
+                       :metamorph/mode :transform}))
+              (:metamorph/data)
+              (ds-mod/column-values->categorical :species)
+              seq
+              )]
 
-  )
-(comment
-
-  (def  ds
-    (ds/->dataset "https://raw.githubusercontent.com/techascent/tech.ml/master/test/data/iris.csv" {:key-fn keyword}))
-
-  (defn pipe-create-fn [options]
-    (morph/pipeline
-     (ds-mm/set-inference-target :species)
-     (ds-mm/categorical->number cf/categorical)
-     (fn [ctx]
-       (assoc ctx
-              :target-ds (cf/target (:metamorph/data ctx))))
-     (ml-mm/model (merge options
-                         {:model-type :smile.classification/random-forest}))))
-
-  (def pipe-options
-    {:trees (gs/categorical [10 50 100 500])
-     :split-rule (gs/categorical [:gini :entropy])})
-
-  (def split-type :kfold)
-  (def split-options {:k 10})
-
-  (def loss-fn loss/classification-loss)
-
-
-
-
-  (def res
-    (evaluate-model ds pipe-create-fn pipe-options split-type split-options loss-fn))
-
-  (def best-fitted-context
-    (-> res first first :fitted-ctx)) ;; ignoring "lowest" loss for th moment
-
-  (def best-pipe-fn
-    (-> res first first :pipe-fn))
-
-
-  (def new-ds (ds/sample ds))
-
-  (best-pipe-fn
-   (merge best-fitted-context
-          {:metamorph/data new-ds
-           :metamorph/mode :transform}))
-
-
-
-
-  (map
-   (fn [{:keys [train test]}]
-     [(ds/row-count train)
-      (ds/row-count test)
-      ]
-
-     )
-   ))
+    (is (= ["versicolor"
+            "versicolor"
+            "virginica"
+            "versicolor"
+            "virginica"
+            "setosa"
+            "virginica"
+            "virginica"
+            "versicolor"
+            "versicolor"]
+           predictions))))
