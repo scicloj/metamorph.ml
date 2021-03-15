7da695df1ab5f282cd7b731204320db2e3235ab1
Carsten Behring <carsten.behring@gmail.com>
2021-03-15 17:03:12 +0100

    re-exported ml relevant fns from dataset as normal and metamorph

---
+++ src/scicloj/metamorph/ml/dataset_metamorph.clj
@@ -0,0 +1,36 @@
+(ns scicloj.metamorph.ml.dataset-metamorph
+  (:require [scicloj.metamorph.ml.dataset]
+            [tech.v3.protocols.dataset :as prot])
+  )
+
+(defn dataset?
+  "Is `ds` a `dataset` type?"
+  [ds]
+  (satisfies? prot/PColumnarDataset ds))
+
+(defmacro build-pipelined-function
+  [f m]
+  (let [args (map (comp vec rest) (:arglists m))
+        doc-string (:doc m)]
+    `(defn ~(symbol (name f)) {:doc ~doc-string}
+       ~@(for [arg args
+               :let [narg (mapv #(if (map? %) 'options %) arg)
+                     [a & r] (split-with (partial not= '&) narg)]]
+           (list narg `(fn [ds#]
+                         (let [ctx# (if (dataset? ds#)
+                                      {:metamorph/data ds#} ds#)]
+                           (assoc ctx# :metamorph/data (apply ~f (ctx# :metamorph/data) ~@a ~(rest r))))))))))
+
+(def ^:private excludes '#{})
+
+(defmacro process-all-api-symbols
+  []
+  (let [ps (ns-publics 'scicloj.metamorph.ml.dataset)]
+    `(do ~@(for [[f v] ps
+                 :when (not (excludes f))
+                 :let [m (meta v)
+                       f (symbol "scicloj.metamorph.ml.dataset" (name f))]]
+             `(build-pipelined-function ~f ~m)))))
+
+(process-all-api-symbols)
+
