4ad494d5ab722a8449d66b6ff86a8db87cf571e8
Carsten Behring <carsten.behring@gmail.com>
2021-06-01 16:23:05 +0200

    added iris

--- src/scicloj/metamorph/ml/toydata.clj
+++ src/scicloj/metamorph/ml/toydata.clj
@@ -3,14 +3,11 @@
             [tech.v3.dataset :as ds]
             [tech.v3.dataset.modelling :as ds-mod]
             [tech.v3.datatype.functional :as dtfn]
-            )
-
-  )
+            ))
 
 (defn sonar-ds []
   (->  (ds/->dataset
         (clojure.java.io/input-stream (clojure.java.io/resource "data/sonar.csv"))
-        ;; "data/sonar.csv"
         {:header-row? false :file-type :csv})
        (tc/rename-columns
         (zipmap
@@ -18,12 +15,8 @@
          (concat
           (map #(keyword (str "x" %)) (range 60))
           [:material]
-          )
+          )))))
 
-         ))))
-
-;; (sonar-ds)
-;;
 
 (defn diabetes-ds []
   (let [data
@@ -31,18 +24,11 @@
             (clojure.java.io/input-stream )
             (ds/->dataset
              {:file-type :csv :gzipped? true :separator " " :header-row? false} )
-            ;; (ds/column-names)
             (ds/rename-columns
              (zipmap
               ( map #(str "column-" %) (range 10))
               [:age :sex :bmi :bp
-               :s1 :s2 :s3 :s4 :s5 :s6]
-
-              )
-             )
-            )
-
-
+               :s1 :s2 :s3 :s4 :s5 :s6])))
 
         targets
         (-> (clojure.java.io/resource "data/diabetes_target.csv")
@@ -52,9 +38,21 @@
             (ds/rename-columns {"column-0" :disease-progression})
             (ds/update-column :disease-progression (fn [col] (map #(Integer/valueOf (Math/round %)) col)))
             )]
-
-
     (->
      (ds/concat data targets)
-     (ds-mod/set-inference-target :disease-progression))
-    ))
+     (ds-mod/set-inference-target :disease-progression))))
+
+
+(-> (clojure.java.io/resource "data/iris.csv")
+    (clojure.java.io/input-stream )
+    (ds/->dataset
+     {:file-type :csv :gzipped? true :header-row? false  :n-initial-skip-rows 1} )
+    (ds/rename-columns
+     (zipmap
+      ( map #(str "column-" %) (range 5))
+      [:sepal_length :sepal_width
+       :petal_length :petal_width
+       :species
+       ]))
+    (ds-mod/set-inference-target :species)
+    (ds/categorical->number [:species] {} :int16))
