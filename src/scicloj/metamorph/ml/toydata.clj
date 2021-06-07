1507d193580db39d872cdbe50d50640c7ce0cf5c
Carsten Behring <carsten.behring@gmail.com>
2021-06-07 22:46:35 +0200

    added bc data

--- src/scicloj/metamorph/ml/toydata.clj
+++ src/scicloj/metamorph/ml/toydata.clj
@@ -3,6 +3,7 @@
             [tech.v3.dataset :as ds]
             [tech.v3.dataset.modelling :as ds-mod]
             [tech.v3.datatype.functional :as dtfn]
+            [camel-snake-kebab.core :as csk]
             ))
 
 (defn sonar-ds []
@@ -56,3 +57,39 @@
          ]))
       (ds-mod/set-inference-target :species)
       (ds/categorical->number [:species] {} :int16)))
+
+
+(defn breast-cancer-ds []
+  (-> (clojure.java.io/resource "data/breast_cancer.csv")
+      (clojure.java.io/input-stream )
+      (ds/->dataset
+       {:file-type :csv :gzipped? true :header-row? false  :n-initial-skip-rows 1} )
+      (ds/rename-columns
+       (zipmap
+        ( map #(str "column-" %) (range 31))
+        (conj
+         (mapv csk/->kebab-case-keyword
+               ["mean radius" "mean texture"
+                "mean perimeter" "mean area"
+                "mean smoothness" "mean compactness"
+                "mean concavity" "mean concave points"
+                "mean symmetry" "mean fractal dimension"
+                "radius error" "texture error"
+                "perimeter error" "area error"
+                "smoothness error" "compactness error"
+                "concavity error" "concave points error"
+                "symmetry error" "fractal dimension error"
+                "worst radius" "worst texture"
+                "worst perimeter" "worst area"
+                "worst smoothness" "worst compactness"
+                "worst concavity" "worst concave points"
+                "worst symmetry" "worst fractal dimension"])
+         :class)))
+
+      (ds/update-column :class (fn [col] (map
+                                         #(case % 0 :malignant 1 :benign )
+                                         col
+                                         ))
+                        )
+      (ds/categorical->number [:class] {} :int16)
+      (ds-mod/set-inference-target :class)))
