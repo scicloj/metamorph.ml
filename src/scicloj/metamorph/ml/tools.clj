fba1897377fd2932a6449538cdd78cf928b525d6
Carsten Behring <carsten.behring@gmail.com>
2021-10-05 00:36:16 +0200

    missing file

---
+++ src/scicloj/metamorph/ml/tools.clj
@@ -0,0 +1,26 @@
+(ns scicloj.metamorph.ml.tools)
+
+(defn dissoc-in
+  "Dissociate a value in a nested assocative structure, identified by a sequence
+  of keys. Any collections left empty by the operation will be dissociated from
+  their containing structures."
+  [m ks]
+  (if-let [[k & ks] (seq ks)]
+    (if (seq ks)
+      (let [v (dissoc-in (get m k) ks)]
+        (if (empty? v)
+          (dissoc m k)
+          (assoc m k v)))
+      (dissoc m k))
+    m))
+
+
+(defn multi-dissoc-in [m kss]
+  (reduce (fn [x y]
+            (dissoc-in x y))
+          m
+          kss))
+
+
+(defn pp-str [x]
+  (with-out-str (clojure.pprint/pprint x)))
