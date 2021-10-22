2d94c6eb8d2bd06396d685842b47ab9c199caf6e
Carsten Behring <carsten.behring@gmail.com>
2021-10-22 11:08:59 +0200

    new test

--- test/scicloj/metamorph/unsupervised_test.clj
+++ test/scicloj/metamorph/unsupervised_test.clj
@@ -25,7 +25,48 @@
         [:model :model-data]))))
 
 
-(deftest test-metricfn
+
+
+
+
+(def  iris
+  (tc/dataset
+   "https://raw.githubusercontent.com/scicloj/metamorph.ml/main/test/data/iris.csv"
+   {:key-fn keyword}))
+
+(defn make-pipe-fn [n]
+  (mm/pipeline
+   (fn [ctx] (assoc ctx :n n))
+   (tc-mm/select-columns :type/float)
+   {:metamorph/id :model}
+   (ml/model {:model-type :fastmath/cluster
+              :clustering-method :k-means
+              :clustering-method-args [n]})))
+
+(deftest distortion
+  (let [eval-result
+        (ml/evaluate-pipelines
+         (map make-pipe-fn (range 2 10))
+         [{:train iris :test (tc/dataset)}]
+         (fn [ctx]
+           (-> ctx :model :model-data :info :distortion))
+         :loss
+         {:return-best-crossvalidation-only false
+          :return-best-pipeline-only false})
+
+        ellbow-data
+        (->>
+         (zipmap
+          (->> eval-result flatten (map #(get-in % [:fit-ctx :n])))
+          (->> eval-result flatten (map #(get-in % [:train-transform :metric]))))
+         (sort-by :n)
+         reverse)]
+
+    (is  (every? true? (map pos?  (vals ellbow-data))))))
+
+
+
+(deftest test-metricfn-collector-fn
     (let [
           iris
           (tc/dataset
@@ -39,19 +80,19 @@
                        (ml/model {:model-type :fastmath/cluster
                                   :clustering-method :k-means
                                   :clustering-method-args [n]})))
-          distortions (atom [])]
 
-      (ml/evaluate-pipelines
-       (map make-pipe (range 2 10))
-       [{:train iris :test (tc/dataset)}]
-       (fn [ctx]
-         (let [
-               n  (-> ctx :model :options :clustering-method-args first)
-               distortion (-> ctx :model :model-data :info :distortion)]
-           (swap! distortions conj {:n n :distortion distortion})
-           0))
-       :loss)
+          distortions (atom [])
+
+          _
+          (ml/evaluate-pipelines
+           (map make-pipe (range 2 10))
+           [{:train iris :test (tc/dataset)}]
+           (fn [ctx]
+             (let [
+                   n  (-> ctx :model :options :clustering-method-args first)
+                   distortion (-> ctx :model :model-data :info :distortion)]
+               (swap! distortions conj {:n n :distortion distortion})
+               0))
+           :loss)]
       (is (= 2 (-> @distortions first :n)))
       (is (= 8 (-> @distortions count)))))
-
-
