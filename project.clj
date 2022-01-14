3712cbd193d2f607c5444f4cdaea19567e03cb44
Carsten Behring <carsten.behring@gmail.com>
2022-01-14 22:33:20 +0100

    replace project.clj with buil.dclj

--- project.clj
+++
@@ -1,8 +0,0 @@
-(defproject scicloj/metamorph.ml "0.5.0-SNAPSHOT"
-  :description "Machine learning model evaluations using context based pipelines."
-  :url "https://github.com/scicloj/metamorph"
-  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :plugins [[lein-tools-deps "0.4.5"]]
-  :middleware [lein-tools-deps.plugin/resolve-dependencies-with-deps-edn]
-  :lein-tools-deps/config {:config-files [:install :user :project]})
