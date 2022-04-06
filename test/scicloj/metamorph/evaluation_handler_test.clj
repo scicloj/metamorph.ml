f25dc08ec2ea7c824b158d5f6c51e1640cf4018a
Andrew Foltz-Morrison <afoltzm@gmail.com>
2022-04-06 14:54:02 -0400

    Refer to ml test file in repo

--- test/scicloj/metamorph/evaluation_handler_test.clj
+++ test/scicloj/metamorph/evaluation_handler_test.clj
@@ -18,9 +18,8 @@
         (get-source-information [[:scicloj.metamorph.ml-test/do-xxx]
                                  [:tech.v3.dataset.metamorph/set-inference-target [:species]]
                                  [::tc-pipe/add-column]]
-
                                 (find-ns 'scicloj.metamorph.ml-test)
-                                "/home/carsten/Dropbox/sources/metamorph.ml/test/scicloj/metamorph/ml_test.clj")]
+                                "test/scicloj/metamorph/ml_test.clj")]
 
     (is (=  "(defn do-xxx [col] col)\n"
             (-> source-info :fn-sources (get 'scicloj.metamorph.ml-test/do-xxx) :code-local-source)))))
