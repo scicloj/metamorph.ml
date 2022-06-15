6b9d5e5f07b5e069f037569d320103133001bdde
Carsten Behring <carsten.behring@gmail.com>
2022-06-15 23:42:52 +0200

    added docu and dbueg

--- src/scicloj/metamorph/ml/ensemble.clj
+++ src/scicloj/metamorph/ml/ensemble.clj
@@ -17,6 +17,10 @@
 
 
 (defn  ensemble-pipe [pipes]
+  "Creates an ensemble pipeline function out of various pipelines. The different predictions
+   get combined via majority voting.
+   Can be used in the same way as any other pipeline.
+"
   (def pipes pipes)
   (morph/pipeline
    {:metamorph/id :ensemble}
@@ -43,69 +47,69 @@
          :transform
          (let [
 
-                 _ (def ctx ctx)
-                 _ (def id id)
+               _ (def ctx ctx)
+               _ (def id id)
 
 
-                 target-column  (-> ctx (get id) :target-column)
+               target-column  (-> ctx (get id) :target-column)
 
-                 _ (def target-column target-column)
+               _ (def target-column target-column)
 
-                 transformed-ctxs
-                 (map
-                  (fn [pipe-key pipe] (morph/transform-pipe data pipe (-> ctx (get id) :fitted-ctxs pipe-key)))
-                  pipe-keys
-                  pipes)
-                 _ (def transformed-ctxs transformed-ctxs)
-
-
-                 predictions
-                 (map
-                  #(cf/prediction (get % :metamorph/data))
-                  transformed-ctxs)
-                 _ (def predictions predictions)
+               transformed-ctxs
+               (map
+                (fn [pipe-key pipe] (morph/transform-pipe data pipe (-> ctx (get id) :fitted-ctxs pipe-key)))
+                pipe-keys
+                pipes)
+               _ (def transformed-ctxs transformed-ctxs)
 
 
-                 columns
-                 (map-indexed
-                  (fn [index prediction]
-                    (ds/new-column (keyword (str "model-" index)) (get prediction target-column)))
-                  predictions)
-                 _ (def columns columns)
+               predictions
+               (map
+                #(cf/prediction (get % :metamorph/data))
+                transformed-ctxs)
+               _ (def predictions predictions)
 
 
-                 target-ds (-> transformed-ctxs first :model :scicloj.metamorph.ml/target-ds)
-                 _ (def target-ds target-ds)
-
-                 target-ds-meta (-> target-ds :transported meta)
-                 _ (def target-ds-meta target-ds-meta)
-
-
-                 prediction-ds (-> (ds/new-dataset columns)
-
-                                   (tc/add-column target-column
-                                                  (fn [ds]
-                                                    (->> ds
-                                                         tc/rows
-                                                         (map majority))))
-                                   (ds/assoc-metadata [target-column]
-                                                      :column-type :prediction
-                                                      ;:categorical? (target-ds-meta :categorical?)
-                                                      :categorical-map (target-ds-meta :categorical-map)))]
+               columns
+               (map-indexed
+                (fn [index prediction]
+                  (ds/new-column (keyword (str "model-" index)) (get prediction target-column)))
+                predictions)
+               _ (def columns columns)
 
 
+               target-ds (-> transformed-ctxs first :model :scicloj.metamorph.ml/target-ds)
+               _ (def target-ds target-ds)
 
-             (def prediction-ds prediction-ds)
-             (assoc ctx
-                    ;; :scicloj.metamorph.ml/feature-ds (-> transformed-ctxs first :model :scicloj.metamorph.ml/feature-ds)
-                    :model {:scicloj.metamorph.ml/target-ds target-ds}
+               target-ds-meta (-> target-ds :transported meta)
+               _ (def target-ds-meta target-ds-meta)
 
-                    :metamorph/data prediction-ds
-                    id
-                    (->>
-                     (map
-                      (fn [pipe-key ctx]
-                        (hash-map pipe-key ctx))
-                      pipe-keys
-                      transformed-ctxs)
-                     (apply merge)))))))))
+
+               prediction-ds (-> (ds/new-dataset columns)
+
+                                 (tc/add-column target-column
+                                                (fn [ds]
+                                                  (->> ds
+                                                       tc/rows
+                                                       (map majority))))
+                                 (ds/assoc-metadata [target-column]
+                                                    :column-type :prediction
+                                        ;:categorical? (target-ds-meta :categorical?)
+                                                    :categorical-map (target-ds-meta :categorical-map)))]
+
+
+
+           (def prediction-ds prediction-ds)
+           (assoc ctx
+                  ;; :scicloj.metamorph.ml/feature-ds (-> transformed-ctxs first :model :scicloj.metamorph.ml/feature-ds)
+                  :model {:scicloj.metamorph.ml/target-ds target-ds}
+
+                  :metamorph/data prediction-ds
+                  id
+                  (->>
+                   (map
+                    (fn [pipe-key ctx]
+                      (hash-map pipe-key ctx))
+                    pipe-keys
+                    transformed-ctxs)
+                   (apply merge)))))))))
