a5a35f562cf4a7d524b79876e8f3ead0f742e050
Carsten Behring <carsten.behring@gmail.com>
2022-12-12 08:51:21 +0100

    use hanami for lc viz

--- src/scicloj/metamorph/ml/viz.clj
+++ src/scicloj/metamorph/ml/viz.clj
@@ -1,59 +1,57 @@
 (ns scicloj.metamorph.ml.viz
-  (:require [tablecloth.api :as tc]))
-
-(defn learning-curve-viz [learning-curve-data]
-
-  (let [plot-data
-        (tc/pivot->longer learning-curve-data [:metric-test :metric-train]
-                        {:value-column-name :metric
-                         :target-columns :train-test-metric})]
-        
-
-    {:$schema "https://vega.github.io/schema/vega-lite/v5.json"
-     :width 500
-     :height 500
-     :config {
-              "mark" {"tooltip" nil}}
-     :data {:values (tc/rows plot-data :as-maps)}
-     :Title "Learning Curve"
-     :layer [{
-              :mark :errorband
-              :encoding {
-                         :color {:value "orange"}
-                         :y {
-                             :field "metric-train-min"
-                             :type "quantitative"
-                             :title "METRIC"
-                             :scale {"zero" true}}
+  (:require
+            [aerial.hanami.templates :as ht]))
 
 
-                         :y2 {:field "metric-train-max"
-                              :legend nil}
-                         :x {:field "train-ds-size"
-                             :title "Traininig instances"}}}
-             {
-              :mark :errorband
-              :encoding {
-                         :color {:value "blue"}
-                         :y {
-                             :field "metric-test-min"
-                             :type "quantitative"
-                             :title "METRIC"
-                             :legend nil
-                             :scale {:zero true}}
+
+(def errorband-encoding-train
+  (assoc
+   ht/xy-encoding
+   :aerial.hanami.templates/defaults
+   {:Y "metric-train-min"
+    :X "train-ds-size"
+    :XTYPE "nominal"}
+   :color {:value "orange"}
+   :y2 {:field "metric-train-max"
+        :legend nil}))
+
+(def errorband-encoding-test
+  (assoc
+   ht/xy-encoding
+   :aerial.hanami.templates/defaults
+   {:Y "metric-test-min"
+    :X "train-ds-size"
+    :XTYPE "nominal"}
+   :color {:value "blue"}
+   :y2 {:field "metric-test-max"
+        :legend nil}))
+
+(def metric-encoding
+  (assoc
+   ht/xy-encoding
+   :aerial.hanami.templates/defaults
+   {:X "train-ds-size"
+    :XTYPE "nominal"
+    :Y "metric"
+    :YTYPE "quantitative"}
+   :color {:field "train-test-metric"
+           :type "nominal"
+           :legend {"labelExpr" "datum.label == 'metric-test' ? 'Cross validation metric' : datum.label == 'metric-train' ? 'Training score' : ''  "}}))
 
 
-                         :y2 {:field "metric-test-max"
-                              :legend nil},
-                         :x {:field "train-ds-size"}}},
-             {
-              :encoding
-              {:color {:field "train-test-metric" :type "nominal"
+(def learning-curve-spec
+  (assoc ht/layer-chart
+         :aerial.hanami.templates/defaults
+         {:TITLE "Learning Curve"
+          :XTITLE "Training size"
+          :YTITLE "metric"}
+         :layer [{:mark :errorband
+                  :encoding errorband-encoding-train}
+                 {:mark :errorband
+                  :encoding errorband-encoding-test}
+                 (assoc ht/line-layer
+                        :aerial.hanami.templates/defaults
+                        {:ENCODING metric-encoding})]))
 
-                       :legend {"labelExpr" "datum.label == 'metric-test' ? 'Cross validation metric' : datum.label == 'metric-train' ? 'Training score' : ''  "}}
 
-               :x {:field "train-ds-size"}
-               :y {:field "metric"
-                   :type "quantitative"}}
 
-              :mark {:point true :type "line"}}]}))
