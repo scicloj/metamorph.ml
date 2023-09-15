1cf6bdc53eb21bd8a94c23e57081089f20b70337
Carsten Behring <carsten.behring@gmail.com>
2023-09-15 16:33:47 +0200

    added residula plots - WIP

--- notebooks/learning_curve.clj
+++ notebooks/learning_curve.clj
@@ -8,9 +8,15 @@
    [tablecloth.api :as tc]
    [scicloj.metamorph.ml.loss]
    [scicloj.ml.smile.classification]
+   [scicloj.ml.smile.regression]
    [tablecloth.pipeline :as tc-mm]
    [tech.v3.dataset]
-   [tech.v3.dataset.metamorph :as mds]))
+   [scicloj.metamorph.ml.toydata :as toydata]
+   [tech.v3.dataset.metamorph :as mds]
+   [aerial.hanami.common :as hc]
+   [aerial.hanami.templates :as ht]
+   [tech.v3.dataset.categorical :as ds-cat]
+   [tech.v3.dataset.modelling :as ds-mod]))
 
 (comment
   (clerk/clear-cache!)
@@ -20,7 +26,7 @@
 ^{:nextjournal.clerk/viewer :table
   :nextjournal.clerk/opts {:page-size 5}}
 
-(def titanic-train
+(def titanic-data
   (->
    (tech.v3.dataset/->dataset "https://github.com/scicloj/metamorph-examples/raw/main/data/titanic/train.csv"
                               {:key-fn keyword})
@@ -42,17 +48,40 @@
    (scicloj.metamorph.ml/model {:model-type model-type})))
 
 
+(def logistic-regression-pipe-fn (make-pipe-fn :smile.classification/logistic-regression))
+
+^{:nextjournal.clerk/viewer :vega-lite}
+(ml-viz/learnining-curve
+ titanic-data
+ logistic-regression-pipe-fn)
+
+
+
 (def train-sizes
   [ 0.04 0.05 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1])
 
+
+
+^{:nextjournal.clerk/viewer :vega-lite}
+(ml-viz/learning-curve
+ titanic-data
+ (make-pipe-fn :smile.classification/logistic-regression)
+ {:train-sizes train-sizes
+  :k 3
+  :metric-fn scicloj.metamorph.ml.loss/classification-accuracy
+  :loss-or-accuracy :accuracy}
+ {})
+
+
 ^{:nextjournal.clerk/viewer :table}
 (def lc-df
-  (lc/learning-curve titanic-train
+  (lc/learning-curve titanic-data
                      (make-pipe-fn :smile.classification/logistic-regression)
                      train-sizes
                      {:k 3
                       :metric-fn scicloj.metamorph.ml.loss/classification-accuracy
                       :loss-or-accuracy :accuracy}))
+
 
 
 
@@ -65,34 +94,233 @@
 (def lc-vl-spec
    (ml-viz/learning-curve-spec lc-vl-data))
 
+
+
 ^{:nextjournal.clerk/viewer :vega-lite}
 (ml-viz/apply-xform-kvs lc-vl-spec {:TITLE "Learning Curve"
                                       :YTITLE "Accuracy"})
 
 
+
+
 ^{:nextjournal.clerk/viewer :vega-lite}
-(ml-viz/learnining-curve
- titanic-train
- (make-pipe-fn :smile.classification/logistic-regression)
- train-sizes
- {:k 3
+(ml-viz/learning-curve
+ titanic-data
+ logistic-regression-pipe-fn
+
+ {:train-sizes [ 0.04 0.05 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1]
+  :k 3
   :metric-fn scicloj.metamorph.ml.loss/classification-accuracy
   :loss-or-accuracy :accuracy}
+
  {:YSCALE {:zero false}
   :TITLE "Learning Curve"
   :YTITLE "Accuracy"})
 
 
 ^{:nextjournal.clerk/viewer :vega-lite}
-(ml-viz/learnining-curve
- titanic-train
+(ml-viz/learning-curve
+ titanic-data
  (make-pipe-fn :smile.classification/logistic-regression)
- train-sizes
- {:k 3
+
+ {:train-sizes [ 0.04 0.05 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1]
+  :k 3
   :metric-fn scicloj.metamorph.ml.loss/classification-accuracy
   :loss-or-accuracy :accuracy}
- {:YSCALE {:zero false}
+
+ {:YSCALE {:zero true}
   :TRAIN-COLOR "green"
   :TEST-COLOR "red"
-  :TITLE "Learning Curve"
+  :XTITLE "Learning Curve"
   :YTITLE "Accuracy"})
+
+(def spec
+  (ml-viz/learning-curve
+
+   titanic-data
+
+   logistic-regression-pipe-fn
+
+
+   {:train-sizes [ 0.04 0.05 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1]
+    :k 3
+    :metric-fn scicloj.metamorph.ml.loss/classification-accuracy
+    :loss-or-accuracy :accuracy}
+
+   {:YSCALE {:zero true}
+    :TRAIN-COLOR "green"
+    :TEST-COLOR "red"
+    :TITLE "Learning Curve"
+    :XTITLE "Training size"
+    :YTITLE "Accuracy"}))
+
+
+(def text-spec
+  {:mark {:type "text"
+          :fontSize 170
+          :angle 29
+          :color "grey"}
+   :data {:values
+          [{"a" 300, "b" 0.72, "label" "Draft"}]}
+   :encoding {:text {:field "label"}
+                     
+              :x {:field "a"
+                  :axis true
+                  :type "quantitative"
+                  :scale {:domain [0 600]}}
+
+              :y {:field "b"
+                  :type "quantitative"
+                  :scale {:domain [0.6 0.9]}}}})
+
+
+^{:nextjournal.clerk/viewer :vega-lite}
+text-spec
+
+^{:nextjournal.clerk/viewer :vega-lite}
+(-> spec
+    ( update-in [:layer] concat [text-spec]))
+
+
+(ml-viz/residual-plot-spec)
+
+
+
+
+^{:nextjournal.clerk/viewer :vega-lite}
+(ml-viz/residual-plot
+ (tc/dataset
+  [
+   {:x 1 :y 1   :prediction 2}
+   {:x 2 :y 1.5 :prediction 3}
+   {:x 3 :y -1  :prediction 1}])
+ {:RESIDUE-COLOR "orange"
+  :LINE-COLOR "black"
+  :STROKEWIDTH 5})
+
+
+(def diabetes
+  (->
+   (scicloj.metamorph.ml.toydata/diabetes-ds)))
+   ;; (tc/select-columns [:age :disease-progression])
+
+
+
+(def pipe-fn
+  (mm/pipeline
+   {:metamorph/id :model}
+   (ml/model {:model-type  :smile.regression/ordinary-least-square})))
+
+(def fitted-context
+  (mm/fit-pipe
+   diabetes
+   pipe-fn))
+
+
+(def model
+  (ml/thaw-model (-> fitted-context :model)))
+
+
+
+^{:nextjournal.clerk/viewer :vega-lite}
+(ml-viz/residual-plot
+ (tc/dataset
+         {:x (:age diabetes)
+          :prediction (:disease-progression diabetes)
+          :y (.fittedValues model)})
+
+ {:LINE ht/RMV
+  :XTITLE "age"
+  :YTITLE "diabetes progression"
+  :RESIDUAL-COLOR "orange"
+  :LINE-COLOR "black"
+  :STROKEWIDTH 1})
+
+
+^{:nextjournal.clerk/viewer :vega-lite}
+(ml-viz/residual-plot
+ (tc/dataset
+  {:x (.fittedValues model)
+
+   :y (:disease-progression diabetes)})
+
+ {:XTITLE "Predicted value for disease progression"
+  :YTITLE "Actual value for disease progression"
+  :LINE (hc/xform  ht/line-chart :X :x :Y :x :MCOLOR "red")
+  :RESIDUAL ht/RMV
+  :STROKEWIDTH 1})
+
+
+(defn residuals-plot [pipe-fn dataset]
+  (let [inference-colum (first (ds-mod/inference-target-column-names dataset))
+
+        fitted-context
+        (mm/fit-pipe
+         dataset
+         pipe-fn)
+
+        transformed-ctx
+        (mm/transform-pipe dataset pipe-fn fitted-context)
+
+        prediction (-> transformed-ctx :metamorph/data (get  inference-colum))
+        observed (get dataset inference-colum)]
+
+    (ml-viz/residual-plot
+     (tc/dataset
+      {:x prediction
+       :y (map - observed prediction)
+       :zero (repeat (tc/row-count dataset) 0)})
+     {:XTITLE "Predicted value"
+      :YTITLE "Actual value"
+      :LINE (hc/xform  ht/line-chart :X :x :Y :zero :MCOLOR "red")
+      :RESIDUAL ht/RMV})))
+     
+
+
+
+(defn residuals-plot-2 [pipe-fn fitted-ctx dataset]
+  (let [inference-colum (first (ds-mod/inference-target-column-names dataset))
+
+
+        transformed-ctx
+        (mm/transform-pipe dataset pipe-fn fitted-ctx)
+
+        prediction (-> transformed-ctx :metamorph/data (get  inference-colum))
+        observed (get dataset inference-colum)]
+
+    (ml-viz/residual-plot
+     (tc/dataset
+      {:x prediction
+       :y (map - observed prediction)
+       :zero (repeat (tc/row-count dataset) 0)})
+     {:XTITLE "Predicted value"
+      :YTITLE "Actual value"
+      :LINE (hc/xform  ht/line-chart :X :x :Y :zero :MCOLOR "red")
+      :RESIDUAL ht/RMV})))
+
+
+
+
+^{:nextjournal.clerk/viewer :vega-lite}
+(residuals-plot
+ (mm/pipeline (ml/model {:model-type  :smile.regression/ordinary-least-square}))
+ diabetes)
+
+
+
+^{:nextjournal.clerk/viewer :vega-lite}
+(let [pipe-fn (mm/pipeline (ml/model {:model-type  :smile.regression/ordinary-least-square}))
+      fitted-context (mm/fit-pipe diabetes pipe-fn)]
+  (residuals-plot-2 pipe-fn fitted-context diabetes))
+
+
+(defn residuals-plot-transformar [ctx]
+  ctx)
+
+(->
+ (mm/fit-pipe
+  diabetes
+  (mm/pipeline
+   (ml/model {:model-type  :smile.regression/ordinary-least-square})
+   residuals-plot-transformar))
+ :metamorph/plot-data)
--- src/scicloj/metamorph/ml/viz.clj
+++ src/scicloj/metamorph/ml/viz.clj
@@ -1,11 +1,12 @@
 (ns scicloj.metamorph.ml.viz
   (:require
-   [tablecloth.api :as tc]
-   [clojure.math :as math]
-   [tech.v3.datatype.functional :as fun]
-   [scicloj.metamorph.ml.learning-curve]
+   [aerial.hanami.common :as hc]
    [aerial.hanami.templates :as ht]
-   [aerial.hanami.common :as hc]))
+   [clojure.math :as math]
+   [scicloj.metamorph.ml.learning-curve]
+   [scicloj.metamorph.ml.loss :as loss]
+   [tablecloth.api :as tc]
+   [tech.v3.datatype.functional :as fun]))
 
 
 
@@ -42,7 +43,8 @@
    :color {:field "train-test-metric"
            :type "nominal"
            :scale {:range [:TRAIN-COLOR :TEST-COLOR]}
-           :legend {"labelExpr" "datum.label == 'metric-test' ? 'Cross validation metric' : datum.label == 'metric-train' ? 'Training score' : ''  "}}))
+           :legend {"labelExpr" "datum.label == 'metric-test' ? 'Cross validation metric' :
+                                 datum.label == 'metric-train' ? 'Training score' : ''  "}}))
 
 
 (def _learning-curve-spec
@@ -50,9 +52,8 @@
          :aerial.hanami.templates/defaults
          {:TITLE "Learning Curve"
           :XTITLE "Training size"
-          :YTITLE "metric"
-          :TRAIN-COLOR "blue"
-          :TEST-COLOR "orange"}
+          :YTITLE "metric"}
+          
          :layer [{:mark :errorband
                   :encoding errorband-encoding-train}
                  {:mark :errorband
@@ -96,6 +97,8 @@
 (defn learning-curve-spec [lc-vl-data]
   (assoc _learning-curve-spec
          :aerial.hanami.templates/defaults {
+                                            :TRAIN-COLOR "blue"
+                                            :TEST-COLOR "orange"
                                             :VALDATA
                                             (-> lc-vl-data
                                                 (tc/pivot->longer [:metric-test :metric-train]
@@ -104,8 +107,8 @@
                                                 (tc/rows :as-maps))}))
 
 
-(defn learnining-curve
-  "Generates a learnining curve.
+(defn learning-curve
+  "Generates a learnining curve plot.
 
   The functions splits  the dataset  in a fixed size test set
   and increasingly larger  training sets. A model is trained at each
@@ -113,25 +116,92 @@
 
   Returns a vega lite spec of the learninig curve plot.
 
-  `dataset` the TMD dataset to use
+  `dataset` The TMD dataset to use
+  `pipe-fn` The metamorph pipleine to use for learning
   `train-sizes` vector of double from 0 to 1, controlling the sizes of the training data.
-  `lc-opts`
+  `lc-opts` Options to create the learnining curve data
      `k` At each step a k cross-validation is done
      `metric-fn` the metric to use for evaluation the model
      `loss-or-accuracy`   If the metric-fn calculates :loss or :accuracy
-  `hanami-opts` Options passed to hanami to control the plot
+     Defaults are k=3 and `:accuracy`
+  `hanami-kvs` Hanami substitution keys passed to hanami to control the learninig curve plot.
+     Standard keys are allowed and additionaly the plot specific keys:
+     `:TRAIN-COLOR`  Color of the train curve
+     `:TEST-COLOR`   Color of the test curve
   "
-  ([dataset pipe-fn train-sizes
-    lc-opts hanami-opts]
+  ([dataset pipe-fn
+    lc-opts hanami-kvs]
    (->
     (scicloj.metamorph.ml.learning-curve/learning-curve
      dataset
      pipe-fn
-     train-sizes lc-opts)
+     (:train-sizes lc-opts) lc-opts)
     (learning-curve-vl-data)
     (learning-curve-spec)
-    (apply-xform-kvs hanami-opts)))
+    (apply-xform-kvs hanami-kvs)))
   ([dataset pipe-fn]
-   (learnining-curve dataset pipe-fn
-                     [0.1 0.325 0.55 0.775 1]
-                     {} {})))
+   (learning-curve dataset pipe-fn
+                     {:train-sizes [0.1 0.325 0.55 0.775 1]
+                      :k 3
+                      :metric-fn loss/classification-accuracy
+                      :loss-or-accuracy :accuracy}
+                     {})))
+
+
+
+(def residual-plot-chart
+  (assoc ht/layer-chart
+         :aerial.hanami.templates/defaults
+         {:XSCALE {:zero false} :YSCALE {:zero false}
+          :RESIDUAL hc/RMV
+          :LINE hc/RMV
+          :POINTS ht/point-layer}
+         ;; :STANDARD (hc/xform ht/line-chart :DATA :REG-DATA :MCOLOR "blue")
+
+         :encoding ht/xy-encoding
+         :layer [:RESIDUAL :POINTS :LINE])) ;; :STANDARD
+
+
+
+(def residual-rule
+  {:mark (merge ht/mark-base {:type "rule"
+                              :strokeWidth :STROKEWIDTH})
+   :encoding (assoc ht/xy-encoding
+
+                    :y2 {:field :Y2 :type :Y2TYPE :bin :Y2BIN
+                         :axis :Y2AXIS :scale :Y2SCALE :sort :Y2SORT
+                         :aggregate :Y2AGG})
+   :aerial.hanami.templates/defaults
+   {:MCOLOR :RESIDUAL-COLOR
+    :XSCALE {:zero false} :YSCALE {:zero false} :Y2SCALE {:zero false}
+    :Y2 :prediction :Y2TYPE :quantitative
+    :Y2BIN hc/RMV :Y2AXIS hc/RMV :Y2SORT hc/RMV :Y2AGG hc/RMV}})
+
+;; (defn residual-plot-spec [data]
+;;   (assoc residual-plot-chart
+;;          :aerial.hanami.templates/defaults))
+         
+
+(def my-line-chart
+  (assoc ht/view-base
+         :mark (merge  ht/mark-base {:type "line"
+                                     :strokeWidth :STROKEWIDTH})))
+
+(defn residual-plot-spec []
+  (assoc-in residual-plot-chart
+            [:aerial.hanami.templates/defaults]
+            (merge (residual-plot-chart :aerial.hanami.templates/defaults)
+                   {:LINE-COLOR "blue"
+                    :RESIDUAL-COLOR "red"
+                    :STROKEWIDTH ht/RMV
+                    :RESIDUAL residual-rule
+                    :LINE (hc/xform  my-line-chart :MCOLOR :LINE-COLOR)}))) ;;
+
+
+
+(defn residual-plot [dataset hanami-opts]
+  (->
+   (residual-plot-spec)
+   (apply-xform-kvs (assoc
+                     hanami-opts
+                     :DATA (tc/rows dataset :as-maps)))))
