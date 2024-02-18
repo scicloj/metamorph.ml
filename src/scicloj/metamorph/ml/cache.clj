d604cc6c2094d4824b86f9577969416944c73cf3
Carsten Behring <carsten.behring@gmail.com>
2024-02-18 21:05:26 +0100

    cleaned up work on persisted map

--- src/scicloj/metamorph/ml/cache.clj
+++ src/scicloj/metamorph/ml/cache.clj
@@ -1,97 +1,20 @@
 (ns scicloj.metamorph.ml.cache
   (:require [taoensso.nippy :as nippy]
-
             [clojure.java.io :as io]
-            [scicloj.metamorph.ml :as ml]
-            [buddy.core.hash :as hash]
-
-
-
-            [clojure.core.cache.wrapped :as wcache]
-
-            [buddy.core.codecs :as codecs]
             [clojure.string :as str]
             [potemkin]))
 
-
-
-
-(defn- k->sha256 [k]
-  (-> k
-      str
-      (hash/sha256)
-      (codecs/bytes->hex)))
-
+(defn ->cache-file [dir k]
+  (io/file (format "%s/%s.nippy" dir k)))
 
 (defn- stream-bytes [is]
-    (let [baos (java.io.ByteArrayOutputStream.)]
-      (io/copy is baos)
-      (.toByteArray baos)))
+  (let [baos (java.io.ByteArrayOutputStream.)]
+    (io/copy is baos)
+    (.toByteArray baos)))
 
-
-
-(defn caching-train
-  [wcache dataset options]
-  (let [k (k->sha256
-           {:op :train
-            :dataset dataset
-            :options options})]
-    (wcache/lookup-or-miss
-     wcache
-     k
-     (fn [item]
-
-       (let [train-result (ml/train
-                           dataset
-                           options)
-
-             wrapped {:model-wrapper train-result
-                      :hash-train-inputs (str (hash k))}
-             bytes (nippy/freeze wrapped)]
-         (println :bytes bytes)
-         wrapped)))))
-
-
-(defn caching-predict [wcache dataset wrapped-model]
-  (let [k
-        (k->sha256
-
-         {:op :predict
-          :hash-train-inputs (:hash-train-inputs wrapped-model)
-          :hash-ds (str (hash dataset))})
-        model (:model-wrapper wrapped-model)]
-
-
-    (wcache/lookup-or-miss
-     wcache
-     k
-     (fn [item]
-
-       (let [predict-result
-             (ml/predict dataset model)
-             bytes (nippy/freeze predict-result)]
-         (println :bytes bytes)
-         predict-result)))))
-
-
-
-
-(defn- item->key [item]
-
-
-  (if (= :train (:op item))
-    (k->sha256
-     {:op :train
-      :options (:options item)
-      :hash-ds (hash (:dataset item))})
-    (k->sha256
-     {:op :predict
-      :hash-train-inputs (:hash-train-inputs (:model-wrapper item))
-      :hash-ds (hash (:dataset item))})))
-
-
-
-(potemkin/def-map-type DiskMap [write-bytes-fn read-bytes-fn delete-key-fn keys-fn]
+;; A map which "freezes" its values via nippy, and delegates to a set of functions to persist this
+;; however needed
+(potemkin/def-map-type NippyMap [write-bytes-fn read-bytes-fn delete-key-fn keys-fn]
 
   (get [_ k default-value]
        (println :get :k k)
@@ -101,50 +24,75 @@
            default-value
            (nippy/thaw bytes))))
 
-
   (assoc [_ k v]
          (println :assoc :k k)
          (write-bytes-fn k (nippy/freeze v))
-         (DiskMap. write-bytes-fn read-bytes-fn delete-key-fn keys-fn))
+         (NippyMap. write-bytes-fn read-bytes-fn delete-key-fn keys-fn))
 
   (dissoc [_ k]
           (println :dissoc)
           (delete-key-fn k)
-
-          (DiskMap. write-bytes-fn read-bytes-fn delete-key-fn keys-fn))
-
+          (NippyMap. write-bytes-fn read-bytes-fn delete-key-fn keys-fn))
 
   (keys [_]
         (println :keys)
         (keys-fn))
-        
 
 
   (meta [_]
         (println :meta)
-        (DiskMap. write-bytes-fn read-bytes-fn delete-key-fn keys-fn))
+        (NippyMap. write-bytes-fn read-bytes-fn delete-key-fn keys-fn))
 
   (with-meta [_ mta]
     (println :with-meta)
-    (DiskMap. write-bytes-fn read-bytes-fn delete-key-fn keys-fn)))
+    (NippyMap. write-bytes-fn read-bytes-fn delete-key-fn keys-fn)))
 
 
 
 
-(defn fs-persisted-map-factory [dir]
-  (DiskMap. (fn [k bytes]
-              (io/copy
-               bytes
-               (io/file (format "%s/%s.nippy" dir k))))
-            (fn [k]
-              (let [file (io/file (format "%s/%s.nippy"
-                                          dir k))]
-                (when (.exists file)
 
-                  (stream-bytes (io/input-stream file)))))
-            (fn [k] (.delete (io/file (format "%s/%s.nippy" dir k))))
-            (fn [] (->>
+(defn persisted-map-factory
+  "Creates a map (String -> bytes) implementation which get persisted. Can be used as base for
+  clojure.core.cachw.wrapped caches to be passed to function `ml/model`
 
-                    (file-seq (io/file dir))
-                    (filter #(.isFile %))
-                    (map #(-> (.getName %) (str/replace ".nippy" "")))))))
+  It takes 4 method to control what persistence means:
+  write-bytes-fn - [key bytes]  -> nil
+  read-bytes-fn  - [key]        -> byte array
+  delete-key-fn  - [key]        -> nil
+  keys-fn        - []           -> seq of Strings
+
+  The `key` passed is URL and filename safe. It's a hash to the inputs of train / predict
+   The `bytes` is a byte array, and represnts the nippied train/predict result
+  "
+
+  [write-bytes-fn read-bytes-fn delete-key-fn keys-fn]
+  (NippyMap. write-bytes-fn read-bytes-fn delete-key-fn keys-fn))
+
+
+
+
+(defn fs-persisted-map-factory
+  "Creates a map (String -> bytes)  which is persisted in local file system.
+  `dir` is the loation of the store.
+  See `presisted-map-factory`
+  "
+
+  [dir]
+
+  (persisted-map-factory
+
+   (fn [k bytes]
+     (io/copy bytes (->cache-file dir k)))
+
+   (fn [k]
+     (let [file (->cache-file dir k)]
+       (when (.exists file)
+         (stream-bytes (io/input-stream file)))))
+
+   (fn [k] (.delete (->cache-file dir k)))
+
+   (fn []
+     (->> (io/file dir)
+          (file-seq)
+          (filter #(.isFile %))
+          (map #(-> (.getName %) (str/replace ".nippy" "")))))))
