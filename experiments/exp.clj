1261741dee8d16f177bb130daf0977dc5a363795
Carsten Behring <carsten.behring@gmail.com>
2024-02-18 02:31:06 +0100

    use file-based map

--- experiments/exp.clj
+++ experiments/exp.clj
@@ -1,16 +1,17 @@
 (ns exp
   (:require
-   [scicloj.ml.smile.classification]
-   [konserve.filestore :refer [connect-fs-store]]
+   [clojure.core.cache.wrapped :as wcache]
+   [clojure.java.io :as io]
    [scicloj.metamorph.core :as morph]
    [scicloj.metamorph.ml :as ml]
+   [scicloj.metamorph.ml.cache :as cache]
    [scicloj.metamorph.ml.loss :as loss]
+   [scicloj.ml.smile.classification]
    [tablecloth.api :as tc]
-   [tech.v3.dataset :as ds]
    [taoensso.nippy :as nippy]
-   [tech.v3.dataset.modelling :as ds-mod]
-   [konserve.core :as k]
-   [scicloj.metamorph.ml.cache :as cache]))
+   [tech.v3.dataset :as ds]
+   [clojure.string :as str]
+   [tech.v3.dataset.modelling :as ds-mod]))
 
 (def iris
   (->
@@ -46,83 +47,146 @@
                      :seed 12345}))
 (def  store "/tmp/store2")
 
+
+(def dir "/tmp/store2")
+(require '[potemkin])
+(potemkin/def-map-type DiskMap [write-bytes-fn read-bytes-fn]
+
+  (get [_ k default-value]
+       (println :get :k k)
+
+       (let [bytes (read-bytes-fn k)]
+         (if (nil? bytes)
+           default-value
+           (nippy/thaw bytes))))
+
+
+  (assoc [_ k v]
+         (println :assoc :k k)
+
+         (io/copy
+          (nippy/freeze v)
+          (io/file (format "%s/%s.nippy" dir k)))
+         (DiskMap. write-bytes-fn read-bytes-fn))
+
+  (dissoc [_ k]
+          (println :dissoc)
+          (.delete (io/file (format "%s/%s.nippy" dir k)))
+          (DiskMap. write-bytes-fn read-bytes-fn))
+
+
+  (keys [_]
+        (println :keys)
+        (->>
+
+         (file-seq (io/file dir))
+         (filter #(.isFile %))
+         (map #(-> (.getName %) (str/replace ".nippy" "")))))
+
+
+
+
+
+  (meta [_]
+        (println :meta
+                 {}))
+  (with-meta [_ mta]
+    (println :with-meta)
+    (DiskMap. write-bytes-fn read-bytes-fn)))
+
+(def wcache (wcache/fifo-cache-factory (DiskMap. (fn [k bytes]
+                                                   (io/copy
+                                                    bytes
+                                                    (io/file (format "%s/%s.nippy" dir k))))
+                                                 (fn [k]
+                                                   (let [file (io/file (format "%s/%s.nippy"
+                                                                               dir k))]
+                                                     (when (.exists file)
+
+                                                        (cache/stream-bytes (io/input-stream file))))))
+                                      {:threshold 1000}))
+
+
+
 (def  pipe-fn-ada (morph/pipeline
 
                    {:metamorph/id :model} (ml/model {:model-type :smile.classification/ada-boost
                                                      :caching-predict-fn (fn [dataset model]
-                                                                           (cache/caching-predict-nippy store dataset model))
+                                                                           (cache/caching-predict-nippy-2 wcache dataset model))
                                                      :caching-train-fn (fn [dataset options]
-                                                                         (cache/caching-train-nippy store dataset options))})))
+                                                                         (cache/caching-train-nippy-2 wcache dataset options))})))
 
 (def  pipe-fn-lg (morph/pipeline
 
                   {:metamorph/id :model} (ml/model {:model-type :smile.classification/logistic-regression
                                                     :caching-predict-fn (fn [dataset model]
-                                                                          (cache/caching-predict-nippy store dataset model))
+                                                                          (cache/caching-predict-nippy-2 wcache dataset model))
                                                     :caching-train-fn (fn [dataset options]
-                                                                        (cache/caching-train-nippy store dataset options))})))
+                                                                        (cache/caching-train-nippy-2 wcache dataset options))})))
 
 (defn  pipe-fn-rf [trees] (morph/pipeline
 
                            {:metamorph/id :model} (ml/model {:model-type :smile.classification/random-forest
                                                              :trees trees
                                                              :caching-predict-fn (fn [dataset model]
-                                                                                   (cache/caching-predict-nippy store dataset model))
+                                                                                   (cache/caching-predict-nippy-2 wcache dataset model))
                                                              :caching-train-fn (fn [dataset options]
-                                                                                 (cache/caching-train-nippy store dataset options))})))
+                                                                                 (cache/caching-train-nippy-2 wcache dataset options))})))
 (def  pipe-fn-slow (morph/pipeline
                     {:metamorph/id :model} (ml/model {:model-type :slow-model
                                                       :very-slow? true
                                                       :caching-predict-fn (fn [dataset model]
-                                                                           (cache/caching-predict-nippy store dataset model))
+                                                                           (cache/caching-predict-nippy-2 wcache dataset model))
                                                       :caching-train-fn (fn [dataset options]
-                                                                          (cache/caching-train-nippy store dataset options))})))
+                                                                          (cache/caching-train-nippy-2 wcache dataset options))})))
+
+
+(def  evaluation-result
+  (ml/evaluate-pipelines
+   (concat
+
+    (map pipe-fn-rf
+         [10 50 100 150 200 500 750 1000])
+    [pipe-fn-slow pipe-fn-lg pipe-fn-ada])
+   splits
+   loss/classification-accuracy
+
+   :accuracy
+   {}))
+
+(println
+ (-> evaluation-result flatten first :test-transform :mean)
+ (-> evaluation-result flatten first :fit-ctx :model :model-wrapper :options))
+
+
 
 (comment
+  (def  cached-pipe-fn-ada (morph/pipeline
+
+                            {:metamorph/id :model} (ml/model {:model-type :smile.classification/ada-boost
+                                                              :caching-predict-fn (fn [dataset model]
+                                                                                    (cache/caching-predict-nippy-2 wcache dataset model))
+
+                                                              :caching-train-fn (fn [dataset options]
+                                                                                  (cache/caching-train-nippy-2 wcache dataset options))})))
+
+  (def  cached-pipe-fn-slow (morph/pipeline
+                             {:metamorph/id :model} (ml/model {:model-type :slow-model
+                                                               :very-slow? true
+                                                               :caching-predict-fn (fn [dataset model]
+                                                                                     (cache/caching-predict-nippy-2 wcache dataset model))
+                                                               :caching-train-fn (fn [dataset options]
+                                                                                   (cache/caching-train-nippy-2 wcache dataset options))})))
   (def  evaluation-result
     (ml/evaluate-pipelines
-     (concat
-
-      (map pipe-fn-rf
-           [10 50 100 150 200 500 750 1000])
-      [pipe-fn-slow pipe-fn-lg pipe-fn-ada])
+     [cached-pipe-fn-slow cached-pipe-fn-ada]
      splits
      loss/classification-accuracy
 
      :accuracy
      {}))
 
+
   (println
-   (-> evaluation-result flatten first :test-transform :mean)
-   (-> evaluation-result flatten first :fit-ctx :model :model-wrapper :options)))
-
-
-
-;; (def cache (clojure.core.cache/ttl-cache-factory bytes-cache))
-
-(def cache  (cache/bytes-storage-cache-factory "/tmp/store2/"))
-
-(def  cached-pipe-fn-ada (morph/pipeline
-
-                          {:metamorph/id :model} (ml/model {:model-type :smile.classification/ada-boost
-                                                            :caching-predict-fn (fn [dataset model]
-                                                                                  (cache/caching-predict-ccc cache dataset model))
-
-                                                            :caching-train-fn (fn [dataset options]
-                                                                                (cache/caching-train-ccc cache dataset options))})))
-
-(def  cached-pipe-fn-slow (morph/pipeline
-                           {:metamorph/id :model} (ml/model {:model-type :slow-model
-                                                             :very-slow? true
-                                                             :caching-predict-fn (fn [dataset model]
-                                                                                   (cache/caching-predict-ccc cache dataset model))
-                                                             :caching-train-fn (fn [dataset options]
-                                                                                 (cache/caching-train-ccc cache dataset options))})))
-(def  evaluation-result
-  (ml/evaluate-pipelines
-   [cached-pipe-fn-slow cached-pipe-fn-ada]
-   splits
-   loss/classification-accuracy
-
-   :accuracy
-   {}))
+     (-> evaluation-result flatten first :test-transform :mean)
+     (-> evaluation-result flatten first :fit-ctx :model :model-wrapper :options)))
