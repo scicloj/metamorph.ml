56c82356dfb2cc20b3357e36072ad08f794e8289
Carsten Behring <carsten.behring@gmail.com>
2024-10-06 14:44:05 +0000

    added train/predict cache

--- experiments/exp.clj
+++ experiments/exp.clj
@@ -9,9 +9,14 @@
    [tech.v3.dataset :as ds]
    [taoensso.nippy :as nippy]
    [tech.v3.dataset.modelling :as ds-mod]
+   [konserve.core :as k]
    [scicloj.metamorph.ml.cache :as cache]))
 
-(def iris (tc/dataset "https://raw.githubusercontent.com/techascent/tech.ml/master/test/data/iris.csv" {:key-fn keyword}))
+(def iris
+  (->
+   (tc/dataset "https://raw.githubusercontent.com/techascent/tech.ml/master/test/data/iris.csv" {:key-fn keyword})
+   (ds/categorical->number [:species])
+   (ds-mod/set-inference-target :species)))
 
 (ml/define-model! :slow-model
   (fn train
@@ -35,44 +40,43 @@
   {})
 
 (def splits (tc/split->seq
-             (-> iris
-                 (ds/categorical->number [:species]))
+             (-> iris)
+                 
              :kfold {:k 11
-                     :seed 4753}))
-(def  store (connect-fs-store "/tmp/store" :opts {:sync? true}))
+                     :seed 12345}))
+(def  store "/tmp/store2")
 
 (def  pipe-fn-ada (morph/pipeline
-                   (morph/lift ds-mod/set-inference-target :species)
+
                    {:metamorph/id :model} (ml/model {:model-type :smile.classification/ada-boost
                                                      :caching-predict-fn (fn [dataset model]
-                                                                           (cache/caching-predict store dataset model))
+                                                                           (cache/caching-predict-nippy store dataset model))
                                                      :caching-train-fn (fn [dataset options]
-                                                                         (cache/caching-train store dataset options))})))
+                                                                         (cache/caching-train-nippy store dataset options))})))
 
 (def  pipe-fn-lg (morph/pipeline
-                  (morph/lift ds-mod/set-inference-target :species)
+
                   {:metamorph/id :model} (ml/model {:model-type :smile.classification/logistic-regression
                                                     :caching-predict-fn (fn [dataset model]
-                                                                          (cache/caching-predict store dataset model))
+                                                                          (cache/caching-predict-nippy store dataset model))
                                                     :caching-train-fn (fn [dataset options]
-                                                                        (cache/caching-train store dataset options))})))
+                                                                        (cache/caching-train-nippy store dataset options))})))
 
 (defn  pipe-fn-rf [trees] (morph/pipeline
-                           (morph/lift ds-mod/set-inference-target :species)
+
                            {:metamorph/id :model} (ml/model {:model-type :smile.classification/random-forest
                                                              :trees trees
                                                              :caching-predict-fn (fn [dataset model]
-                                                                                   (cache/caching-predict store dataset model))
+                                                                                   (cache/caching-predict-nippy store dataset model))
                                                              :caching-train-fn (fn [dataset options]
-                                                                                 (cache/caching-train store dataset options))})))
+                                                                                 (cache/caching-train-nippy store dataset options))})))
 (def  pipe-fn-slow (morph/pipeline
-                    (morph/lift ds-mod/set-inference-target :species)
                     {:metamorph/id :model} (ml/model {:model-type :slow-model
                                                       :very-slow? true
                                                       :caching-predict-fn (fn [dataset model]
-                                                                           (cache/caching-predict store dataset model))
+                                                                           (cache/caching-predict-nippy store dataset model))
                                                       :caching-train-fn (fn [dataset options]
-                                                                          (cache/caching-train store dataset options))})))
+                                                                          (cache/caching-train-nippy store dataset options))})))
 (def  evaluation-result
  (ml/evaluate-pipelines
   (concat
@@ -87,5 +91,5 @@
   {}))
 
 (println
- (-> evaluation-result flatten first :train-transform :mean)
+ (-> evaluation-result flatten first :test-transform :mean)
  (-> evaluation-result flatten first :fit-ctx :model :model-wrapper :options))
--- src/scicloj/metamorph/ml/cache.clj
+++ src/scicloj/metamorph/ml/cache.clj
@@ -2,7 +2,9 @@
   (:require [taoensso.nippy :as nippy]
             [konserve.core :as k]
             [clojure.java.io :as io]
-            [scicloj.metamorph.ml :as ml]))
+            [scicloj.metamorph.ml :as ml]
+            [buddy.core.hash :as hash]
+            [buddy.core.codecs :as codecs]))
 
 
 (defn- stream-bytes [is]
@@ -11,7 +13,6 @@
     (.toByteArray baos)))
 
 (defn- get-binary [store k]
-
   (k/bget store k
           (fn [{is :input-stream}]
             (nippy/thaw (stream-bytes is)
@@ -21,8 +22,16 @@
               ;; "smile.data.formula.Formula"
           {:sync? true}))
 
-(defn caching-train [store dataset options]
+(defn caching-train-konserve
+  "A variant of ml/train which caches training results.
+   It constructs a `hash` out of:
+      * hash of dataset
+      * hash of options
 
+   and checks if present in 'store'. I fpresent it retunrs teh result,
+  otherwise it class ml/train and stores resukt in store under the key `hash`
+  "
+  [store dataset options]
   (let [hash-ds (str (hash dataset))
         k {:op :train
            :options options
@@ -37,7 +46,7 @@
         wrapped))))
         
         
-(defn caching-predict [store dataset wrapped-model]
+(defn caching-predict-konserve [store dataset wrapped-model]
   (let [k {:op :predict
            :hash-train-inputs (:hash-train-inputs wrapped-model)
            :hash-ds (str (hash dataset))}
@@ -48,3 +57,48 @@
         (k/bassoc store k (nippy/freeze predict-result) {:sync? true})
 
         predict-result))))
+
+
+(defn k->sha256 [k]
+  (-> k
+      str
+      (hash/sha256)
+      (codecs/bytes->hex)))
+
+
+
+(defn caching-train-nippy
+  "A variant of ml/train which caches training results.
+   It constructs a `hash` out of:
+      * hash of dataset
+      * hash of options
+
+   and checks if present in 'store'. I fpresent it retunrs teh result,
+  otherwise it class ml/train and stores resukt in store under the key `hash`
+  "
+  [dir dataset options]
+  (let [hash-ds (str (hash dataset))
+        k {:op :train
+           :options options
+           :hash-ds hash-ds}
+        cache-file (io/file (format "%s/%s.nippy" dir (k->sha256  k)))]
+    (if (.exists cache-file)
+      (nippy/thaw-from-file cache-file {:serializable-allowlist #{"*"}})
+      (let [train-result (ml/train dataset options)
+            wrapped {:model-wrapper train-result
+                     :hash-train-inputs (str (hash k))}]
+        (io/copy (nippy/freeze wrapped) cache-file)
+        wrapped))))
+
+
+(defn caching-predict-nippy [dir dataset wrapped-model]
+  (let [k {:op :predict
+           :hash-train-inputs (:hash-train-inputs wrapped-model)
+           :hash-ds (str (hash dataset))}
+        model (:model-wrapper wrapped-model)
+        cache-file (io/file (format "%s/%s.nippy" dir  (k->sha256 k)))]
+    (if (.exists cache-file)
+      (nippy/thaw-from-file cache-file {:serializable-allowlist #{"*"}})
+      (let [predict-result (ml/predict dataset model)]
+        (nippy/freeze-to-file cache-file predict-result)
+       predict-result))))
