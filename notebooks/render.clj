fc9c2a280662ee6381e0610e28e725ee7310741f
Carsten Behring <carsten.behring@gmail.com>
2024-11-17 10:44:17 +0000

    refactored

--- notebooks/render.clj
+++ notebooks/render.clj
@@ -5,22 +5,17 @@
 
 
 (clerk/build! {:paths ["notebooks/confusionmatrix.clj"
-                       "notebooks/roc_curve.clj"
-                       ]
+                       "notebooks/roc_curve.clj"]
                :package :single-file
-               :out-path "docs/clerk"
-               })
+               :out-path "docs/clerk"})
 
 (clay/make! {:format [:quarto :html]
              :base-source-path "notebooks/"
              :source-path ["austen.clj"
-                           ;"confusionmatrix.clj"
-                           ;"roc_curve.clj"
-                           "clerk_link.clj"
-                           "tidytext.clj"]
+                           "tidytext.clj"
+                           "clerk_link.clj"]
              :base-target-path "docs"
              :book {:title "metamorph.ml topics"}
-               ;; Empty the target directory first:
              :clean-up-target-dir false
              :show false})
 (System/exit 0)
