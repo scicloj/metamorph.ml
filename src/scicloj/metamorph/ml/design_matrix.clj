11568bd4879d89decc9672ed32a45548319785b2
Carsten Behring <carsten.behring@gmail.com>
2025-01-09 15:42:01 +0000

    added support for "returned maps"

--- src/scicloj/metamorph/ml/design_matrix.clj
+++ src/scicloj/metamorph/ml/design_matrix.clj
@@ -1,71 +1,113 @@
 (ns scicloj.metamorph.ml.design-matrix
-  (:require [tablecloth.api :as tc]
-            [clojure.set :as set]
-            [tech.v3.dataset.column-filters :as cf]
-            [tech.v3.dataset :as ds]
-            [tech.v3.dataset.modelling :as ds-mod]
-            [tech.v3.dataset.impl.column :as col-impl]))
+  (:require
+   [clojure.set :as set]
+   [clojure.walk :as cljwalk]
+   [tablecloth.api :as tc]
+   [tech.v3.dataset :as ds]
+   [tech.v3.dataset.column-filters :as cf]
+   [tech.v3.dataset.modelling :as ds-mod]))
+
+(defn- combine-with-dash [arg1 arg2]
+  (let [to-string (fn [x]
+                    (cond
+                      (string? x) x
+                      (keyword? x) (name x)
+                      (symbol? x) (name x)
+                      :else (str x)))
+        combined-str (str (to-string arg1) "-" (to-string arg2))]
+    (cond
+      (keyword? arg1) (keyword combined-str)
+      (symbol? arg1) (symbol combined-str)
+      (string? arg1) combined-str
+      :else combined-str)))  
+
+(defn map-column->columns [ds src-col]
+  (let [columns-ds
+        (tc/dataset (get ds src-col))
+        
+        new-col-names
+        (map #(combine-with-dash src-col %)
+             (tc/column-names columns-ds))
+
+        renamed-columns-ds
+        (tc/rename-columns columns-ds
+                           (zipmap
+                            (tc/column-names columns-ds)
+                            new-col-names))]
+    (->
+     (ds/append-columns ds (tc/columns renamed-columns-ds))
+     (ds/remove-column src-col))))
+
+
+
+
 
 (defn- create-design-matrix-column [ds new-column spec]
+  (let [new-column (if (nil? new-column)
+                     (str spec)
+                     new-column)
 
-  (def ds ds)
-  (def new-column new-column)
-  (def spec spec)
-  
-  
-  
-  (let [cols+params 
-        (flatten (vec (rest spec)))
-        
-        _ (def cols+params cols+params)
+
 
         cols (filterv
               #(tc/has-column? ds %)
-              cols+params)
-        
-        params (filterv
-                #(not (tc/has-column? ds %))
-                cols+params)
 
-        _ (def params params)
-        _ (def cols cols)
+              (distinct
+               (concat (flatten spec)
+                       (when (map? spec)
+                         (flatten
+                          (vals spec))))))
+
+
         syms
         (mapv
-         (fn [_] (gensym))
+         (fn [col] {:sym (gensym)
+                    :col col})
          cols)
-        
-        _ (def syms syms)
+
+        col-sym-m (zipmap  (mapv :col syms)
+                           (mapv :sym syms))
 
         fnn
-        (list 'fn syms 
-              (concat 
-              [(first spec)]
-               params
-               syms))
-
-        _ (def fnn fnn)
-        f (eval fnn)
-
-        _ (def f f)
-        ]
+        (list 'fn (mapv :sym syms)
+              (cljwalk/postwalk-replace col-sym-m spec))
+        f (eval fnn)]
 
     (tc/map-columns ds new-column cols f)))
 
 
-(tc/map-columns ds new-column cols f)
-(map class spec)
+(defn create-design-matrix 
+  "Converts the given dataset into a full numeric dataset.
+   
+   `target-specs` are the specifications how to transform the target varibales
+   `features-specs` are the specifications how to transform the features variables
 
-( (eval fnn) 1 2 3)
+   See  `design_matrix_test.clj` for examples.
+   
+    
+   The followig aliases can be used as part of te spec.
+   Other function needs to be full qualified.
 
-(defn create-design-matrix [ds
-                            targets-specs
-                            feature-specs]
+   clojure.core  can be used without full qailifying te symbols
+   ds             (tech.v3.dataset)
+   tc             (tablecloth.api)
+   tcc            (tablecloth.column.api)
+   
+   "   
+  [ds
+   targets-specs
+   features-specs]
+
+  ;; be sure, they are availbale
+  (alias 'ds 'tech.v3.dataset)
+  (alias 'tc 'tablecloth.api)
+  (alias 'tcc 'tablecloth.column.api)
 
   (let [ ds-columns (tc/column-names ds)
 
         mapping-specs-cols
         (concat targets-specs
-                (->> feature-specs
+                (->> features-specs
                      (map first)
                      (remove nil?)))
 
@@ -81,26 +123,32 @@
                                         (first e2)
                                         (second e2)))
          ds
-         feature-specs)
+         features-specs)
+        
         all-cols (tc/column-names transformed-ds)
 
 
         seperated-ds
         (reduce
-
          (fn [ds col]
-           (if  (-> (get ds col) first sequential?)
-             (tc/array-column->columns ds col {:prefix col})
-             ds))
+           (let [new-ds (if  (-> (get ds col) first sequential?)
+                          (tc/array-column->columns ds col {:prefix col})
+                          ds)
+                 new-ds (if  (-> (get ds col) first map?)
+                          (map-column->columns ds col)
+                          new-ds)]
+             new-ds))
          transformed-ds
-         all-cols)
-
-        design-matrix
-        (-> seperated-ds
-            (ds-mod/set-inference-target targets-specs)
-            (tc/drop-columns columns-to-be-removed)
-            (ds/categorical->number cf/categorical))]
-
+         all-cols)]
 
     
-    design-matrix))
+    (-> seperated-ds
+        (ds-mod/set-inference-target targets-specs)
+        (tc/drop-columns columns-to-be-removed)
+        (ds/categorical->number cf/categorical))
+
+    
+    ))
+
+
+
