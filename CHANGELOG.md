7c7d2660cacbbf5a1fcf04ca23f4d4646c549e78
Carsten Behring <carsten.behring@gmail.com>
2025-02-17 17:38:03 +0000

    1.2beta1.1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,6 @@
+1.2beta1.1
+- fixed build
+
 1.2beta1
 - removed most toy data and forward fns to rdatasets/*
 - additional validation of consitency of predict / cat maps 
--- build.clj
+++ build.clj
@@ -13,7 +13,7 @@
 (def lib 'org.scicloj/metamorph.ml)
 ; alternatively, use MAJOR.MINOR.COMMITS:
 ;; (def version (format "6.2.%s" (b/git-count-revs nil)))
-(def version "1.2beta1")
+(def version "1.2beta1.1")
 (def class-dir "target/classes")
 (def basis (b/create-basis {:project "deps.edn"}))
 (def jar-file (format "target/%s-%s.jar" (name lib) version))
