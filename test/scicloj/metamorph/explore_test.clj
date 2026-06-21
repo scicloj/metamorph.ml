78f9b9549f962048e1f54191e4022d7a08984e43
Carsten Behring <carsten.behring@gmail.com>
2026-06-21 19:22:27 +0000

    wip

--- test/scicloj/metamorph/explore_test.clj
+++ test/scicloj/metamorph/explore_test.clj
@@ -5,16 +5,18 @@
             [scicloj.metamorph.ml.explore :refer [explore-all]]
             [scicloj.metamorph.ml.rdatasets :as rdatasets]
             [scicloj.plotje.api :as pj]
-            [tablecloth.api :as tc])
+            [tablecloth.api :as tc]
+            [scicloj.metamorph.ml.tools :as tools]
+            [hiccup2.core :as h]
+            )
   )
+
 
 
 (deftest explore-all-test
   (let [pinguins
         (->
          (rdatasets/palmerpenguins-penguins)
-
-
          (tc/drop-columns [:rownames])
          (tc/drop-missing [:flipper-length-mm])
          (tc/replace-missing [:sex] :value "__NA__")
@@ -26,11 +28,38 @@
          (some false?
                (map
                 (fn [[code expected-svg]]
-                  (let [
-                        fn (ns-resolve *ns* (symbol (first code)) )
+                  (let [fn (ns-resolve *ns* (symbol (first code)))
                         opts (first (rest code))
                         drawn-svg (pj/plot (apply fn pinguins opts))]
-                    (is (= expected-svg drawn-svg))
-                    ))
+
+                    (spit (io/file "/tmp/expected.svg") (str (h/html expected-svg)))
+                    (spit (io/file "/tmp/drawn.svg")  (str (h/html drawn-svg)))
+                    (is (= expected-svg drawn-svg))))
                 svgs))))))
 
+
+(comment
+  (def pinguins
+    (-> 
+     (rdatasets/palmerpenguins-penguins)
+     (tc/drop-columns [:rownames])
+     (tc/drop-missing [:flipper-length-mm])
+     (tc/replace-missing [:sex] :value "__NA__")
+     (tc/add-column :year #(map str (:year %)))
+     ))
+  (map 
+   (fn [code]
+     (def code code)
+   (let [result (eval code)
+         
+         ]
+     
+     )
+     (pr-str )
+     )
+   
+   ['(scicloj.metamorph.ml.explore/explore-all pinguins {:color "skyblue"})])
+
+  )
+  
+  
