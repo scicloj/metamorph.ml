9a910019b24dec6c56e25ba09712c24dc44f32a6
Carsten Behring <carsten.behring@gmail.com>
2026-06-21 19:18:49 +0000

    fixed plots

--- exp/explore.clj
+++ exp/explore.clj
@@ -3,24 +3,25 @@
    [scicloj.metamorph.ml.explore :as explore]
    [scicloj.metamorph.ml.rdatasets :as rdatasets]
    [tablecloth.api :as tc]))
+
+
+(def pinguins
   
+  (->
+   (rdatasets/palmerpenguins-penguins)
 
 
-(let [pinguins
-      (->
-       (rdatasets/palmerpenguins-penguins)
+   (tc/drop-columns [:rownames])
+   (tc/drop-missing [:flipper-length-mm])
+   (tc/replace-missing [:sex] :value "__NA__")
+   (tc/add-column :year #(map str (:year %)))))
+
+; # Explore variables
+
+(explore/explore-all pinguins
+                     {:color "blue"})
 
 
-       (tc/drop-columns [:rownames])
-       (tc/drop-missing [:flipper-length-mm])
-       (tc/replace-missing [:sex] :value "__NA__")
-       (tc/add-column :year #(map str (:year %))))
-      
-      
-      ]
-  (explore/explore-all pinguins 
-                       {}
-                       )
-  )
-
-  
+; # Explore variables vs target  
+(explore/explore-all pinguins
+                     {:target :species})
--- src/scicloj/metamorph/ml/explore.clj
+++ src/scicloj/metamorph/ml/explore.clj
@@ -6,7 +6,8 @@
             [scicloj.plotje.api :as pj]
             [tablecloth.column.api :as tcc]
             [tech.v3.dataset.column-filters :as cf]
-            [tech.v3.dataset :as dataset]))
+            [tech.v3.dataset :as dataset]
+            [tech.v3.dataset.column :as ds-col]))
 
 
 
@@ -20,20 +21,34 @@
 
 
 (defn explore-categorical-var [data variable {:keys [color target]}]
-  (let [;group (if (some? target) target color)
+  ;; (def data data)
+  ;; (def variable variable)
+  ;; (def color color)
+  ;; (def target target)
+
+  (let [group (if (some? target) target color)
         freqs (-> data variable frequencies)
         n-missing (-> data variable tc/select-missing tc/row-count)
         n-unique (count freqs)
+        subtitle (if (some? target)
+                   ""
+                   (format "na = %d, unique = %d" n-missing n-unique))
+
 
         plot-data
         (-> data
             (tc/group-by [target variable])
             (tc/aggregate (fn [ds]
+                            
                             (round-to-precision 2
                                                 (* 100.0
                                                    (/
                                                     (tc/row-count ds)
-                                                    (tc/row-count data)))))
+                                                    (if (some? target)
+                                                      (get freqs (first (get ds variable)))
+                                                      (tc/row-count data))
+                                                    
+                                                    ))))
                           {:default-column-name-prefix :%}))]
     ;; (def plot-data plot-data)
     ;; (def group group)
@@ -44,41 +59,65 @@
     ;; (def color color)
     (->
      plot-data
-     (pj/lay-value-bar variable :% {
-                                    :color color
+     (pj/lay-value-bar variable :% {:color group
                                     :alpha 0.7})
-      (pj/lay-text variable :% {:text :%
-                                 :color "black"
-                                 :align-y :bottom
-                                 :align-x :right
-                                 })
+     ((fn [pose]
+
+        (if (some? target)
+          pose
+          (pj/lay-text pose variable :% {:text :%
+                                         :color "black"
+                                         :align-y :bottom
+                                         :align-x :right}))))
      (pj/options {:title (name variable)
-                  :subtitle (format "na = %d, unique = %d" n-missing n-unique)
+                  :subtitle subtitle
                   :x-label ""})
-     (pj/coord :flip)
-     )))
+     (pj/coord :flip))))
 
 
 
 (defn explore-continous-var [data variable {:keys [color target]}]
-  (let [
-        ;group (if (some? target) target color)
+  (let [group (if (some? target) target color)
         qq-2 (stats/quantile (get data variable) 0.02)
         qq-98 (stats/quantile (get data variable) 0.98)
         n-missing (-> data variable tc/select-missing tc/row-count)
         min (tcc/reduce-min (get data variable))
         max (tcc/reduce-max  (get data variable))
-        mean (tcc/mean  (get data variable))]
+        mean (tcc/mean  (get data variable))
+        subtitle (if (some? target)
+                   ""
+                   (format "na = %d, min = %.2f, max = %.2f" n-missing (double min) (double max)))
+        rule-fn (if (some? target)
+                  (fn [pose] pose)
+                  (fn [pose] (pj/lay-rule-v pose {:x-intercept mean :color "grey" :alpha 0.5})))
+        rug-fn (if (some? target)
+                 (fn [pose] pose)
+                 (fn [pose] (pj/lay-rug pose)))
+        ]
+    ;; (def data data)
+    ;; (def qq-2 qq-2)
+    ;; (def qq-98  qq-98)
+    ;; (def group group)
+    ;; (def variable variable)
+    ;; (def subtitle subtitle)
+    ;; (def rule-fn rule-fn)
     (-> data
-        (tc/select-rows (fn [row]
-                          (and
-                           (>= (get row variable) qq-2)
-                           (<= (get row variable) qq-98))))
-        (pj/pose variable)
-        (pj/lay-density  {:color color}  )
-        (pj/lay-rule-v {:x-intercept mean :color "grey" :alpha 0.5})
+        ;; not doing anything
+        ;; (tc/select-rows (fn [row]
+        ;;                   (and
+        ;;                    (>= (get row variable) qq-2)
+        ;;                    (<= (get row variable) qq-98))))
+    
+        (pj/lay-density variable  {:color group}  )
+
+        (rug-fn
+         )
+        (rule-fn)
+        ;TODO: https://github.com/scicloj/plotje/issues/23
+
+        ;(pj/scale :x {:domain [qq-2 qq-98]})
         (pj/options {:title (name variable)
-                     :subtitle (format "na = %d, min = %.2f, max = %.2f" n-missing (double min) (double max))
+                     :subtitle subtitle
                      :x-label ""}))))
 
 (defn explore-all [data opts]
@@ -91,11 +130,13 @@
      (->>
       (map
        (fn [col]
-         (if (:categorical? (meta col))
-           (explore-categorical-var data (:name (meta col)) defaulted-opts)
-           (explore-continous-var data (:name (meta col)) defaulted-opts)))
+         (when (not (= (ds-col/column-name col) (:target opts)))
+           (if (:categorical? (meta col))
+             (explore-categorical-var data (:name (meta col)) defaulted-opts)
+             (explore-continous-var data (:name (meta col)) defaulted-opts))))
        (->
         (tc/columns data)))
+      (remove nil?)
       (partition-all 2))
      (select-keys defaulted-opts [:height
                                   :width]))))
